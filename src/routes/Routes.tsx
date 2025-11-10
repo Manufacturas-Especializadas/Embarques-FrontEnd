bc9c64b61869eb36b42cb334ce2be10d5111d6a9
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-11-10 11:20:17 -0600

    Ruta de login y registro

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,12 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import { FletesIndex } from "../pages/Fletes/FletesIndex";
 import { ReportsIndex } from "../pages/Reports/ReportsIndex";
+import { Register } from "../pages/Auth/Register";
+import { Login } from "../pages/Auth/Login";
 
 export const MyRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<FletesIndex />} />
             <Route path="/reportes" element={<ReportsIndex />} />
+
+            <Route path="/registro" element={<Register />} />
+            <Route path="/login" element={<Login />} />
         </Routes>
     );
 };
